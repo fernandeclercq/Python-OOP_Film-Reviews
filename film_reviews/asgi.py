374819c98131c7c4bee9cd85460f7e214ee2e5e3
fernandeclercq <fernandomedinadb@hotmail.com>
2022-10-07 10:49:58 +0200

    recreate film_review project

--- film_reviews/asgi.py
+++ film_reviews/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Film_reviews project.
+ASGI config for film_reviews project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Film_reviews.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'film_reviews.settings')
 
 application = get_asgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Film_reviews.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'film_reviews.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
