3079820acadd4757dff17055a9b1f7d02148a2d4
fernandeclercq <fernandomedinadb@hotmail.com>
2022-10-10 12:56:19 +0200

    initialize model film

--- filmreviews/film/views.py
+++ filmreviews/film/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {'name':'Fernando Declercq'})
+    zoek_term = request.GET.get('zoekfilm')
+    return render(request, 'home.html', {'name':'Fernando Declercq', 'zoekTerm': zoek_term})
 
 def over(request):
     return HttpResponse('<h1>Welkom op de over-ons pagina</h1>')
