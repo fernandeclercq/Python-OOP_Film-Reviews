165dde164172b1663971f2e5680b94cdf6e6473f
fernandeclercq <fernandomedinadb@hotmail.com>
2022-10-10 15:01:26 +0200

    finalize nieuws object en model

--- filmreviews/nieuws/models.py
+++ filmreviews/nieuws/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Nieuws(models.Model):
+    koptekst = models.CharField(max_length=200)
+    body = models.TextField()
+    datum = models.DateField()
--- filmreviews/nieuws/views.py
+++ filmreviews/nieuws/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Nieuws
+
 
 # Create your views here.
+
+def nieuws(request):
+    nieuwsberichten = Nieuws.objects.all().order_by('-datum')
+    return render(request, 'nieuws.html', {
+        'nieuwsberichten': nieuwsberichten
+    })
