70434e78b5e112a48d77cea5fb9f5385313df8a3
fernandeclercq <fernandomedinadb@hotmail.com>
2022-10-24 12:06:25 +0200

    add home & over view to film/views

--- filmreviews/filmreviews/urls.py
+++ filmreviews/filmreviews/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from film import views as filmViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', filmViews.home),
+    path('over/', filmViews.over)
 ]
