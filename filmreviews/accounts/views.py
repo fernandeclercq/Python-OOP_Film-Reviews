57e0bcd9a3602cd0a8d717ec311959b06ca9521b
fernandeclercq <fernandomedinadb@hotmail.com>
2022-11-03 23:35:13 +0100

    create views.py for accounts

--- filmreviews/accounts/views.py
+++ filmreviews/accounts/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from .forms import GebruikerRegistratieForm
 from django.contrib.auth.models import User # Het Django user model
 from django.contrib.auth import login # De login functie
 from django.shortcuts import redirect # De redirect functie
 from django.db import IntegrityError
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 
@@ -12,7 +16,7 @@ def registreer(request):
     try:
 
         if request.method == 'GET':
-            return render(request, 'registreer.html', {'form': UserCreationForm})
+            return render(request, 'registreer.html', {'form': GebruikerRegistratieForm})
         else:
             if request.POST['password1'] == request.POST['password2']:
                 user = User.objects.create_user(
@@ -23,7 +27,25 @@ def registreer(request):
                 login(request, user)
                 return redirect('home')
             else:
-                return render(request, 'registreer.html', {'form': UserCreationForm, 'error': "Wachtwoorden matchen niet" })
+                return render(request, 'registreer.html', {'form': GebruikerRegistratieForm, 'error': "Wachtwoorden matchen niet" })
 
     except IntegrityError:
-        return render(request, 'registreer.html', {'form':UserCreationForm, 'error': 'Gebruikersnaam is reeds gebruikt'})
+        return render(request, 'registreer.html', {'form':GebruikerRegistratieForm, 'error': 'Gebruikersnaam is reeds gebruikt'})
+
+def loguit(request):
+    logout(request)
+    return redirect('home')
+
+
+def loginaccount(request):
+    if request.method == 'GET':
+        return render(request, 'login.html', {'form':AuthenticationForm})
+    else:
+        user = authenticate(request,
+        username = request.POST['username'],
+        password = request.POST['password'])
+        if user is None:
+            return render(request, 'login.html', {'form':AuthenticationForm(), 'error':'Gebruikersnaam bestaat niet'})
+        else:
+            login(request, user)
+            return redirect('home')
