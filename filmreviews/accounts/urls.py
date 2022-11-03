a29807cd6bdef2fea858bb3bf06c62ae42523889
fernandeclercq <fernandomedinadb@hotmail.com>
2022-11-03 23:34:49 +0100

    add login, logout en registreer routes

--- filmreviews/accounts/urls.py
+++ filmreviews/accounts/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('registreer/', views.registreer, name='registreer'),
+    path('logout', views.loguit, name='loguit'),
+    path('login/', views.loginaccount, name='login'),
 ]
