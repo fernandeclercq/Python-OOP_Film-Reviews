77f017a9b03f4d2b8d1a2f375c9803e851c9879a
fernandeclercq <fernandomedinadb@hotmail.com>
2022-11-03 22:08:05 +0100

    add account route to urls.py

--- filmreviews/filmreviews/urls.py
+++ filmreviews/filmreviews/urls.py
@@ -1,18 +1,3 @@
-"""filmreviews URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
@@ -29,7 +14,8 @@ urlpatterns = [
     path('', filmViews.home, name='home'),
     path('over/', filmViews.over, name='over'),
     path('film/', include('film.urls')),
-    path('nieuws/', include('nieuws.urls'))
+    path('nieuws/', include('nieuws.urls')),
+    path('accounts/', include('accounts.urls'))
 ]
 
 # Used to show media on pages
